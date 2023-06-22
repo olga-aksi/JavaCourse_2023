1ebd651d4fc0c282594c7f605a29438b1a34741a
olga-aksi <olga-aksi@users.noreply.github.com>
2023-06-22 23:48:14 +0300

    Добавлены тесты для площади квадрата

--- Sandbox/src/test/java/ru/olgaaksi/jvc/sandbox/SquareTests.java
+++ Sandbox/src/test/java/ru/olgaaksi/jvc/sandbox/SquareTests.java
@@ -1,9 +1,12 @@
 package ru.olgaaksi.jvc.sandbox;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class SquareTests {
   @Test
   public void testArea() {
     Square s = new Square(8);
-    assert s.area() == 64;
+    Assert.assertEquals(s.area(), 64.0);
   }
 }
