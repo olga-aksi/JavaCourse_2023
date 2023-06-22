5fe066ef74463d6c88afe50d0bd983dc7fc576c9
olga-aksi <olga-aksi@users.noreply.github.com>
2023-06-22 22:40:26 +0300

    Вызов метода Point в main

--- Sandbox/src/main/java/ru/olgaaksi/jvc/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/olgaaksi/jvc/sandbox/MyFirstProgram.java
@@ -5,9 +5,6 @@ public class MyFirstProgram {
 
     Point p1 = new Point(25, 15);
     Point p2 = new Point(16, 78);
-    System.out.println("Расстояние между точками А и В равно " + distance(p1, p2));
-  }
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p1.lat - p2.lat) * (p1.lat - p2.lat) + (p1.lon - p2.lon) * (p1.lon - p2.lon));
+    System.out.println("Расстояние между точками А и В равно " + p1.distance(p2));
   }
  }
--- Sandbox/src/main/java/ru/olgaaksi/jvc/sandbox/Point.java
+++ Sandbox/src/main/java/ru/olgaaksi/jvc/sandbox/Point.java
@@ -8,5 +8,8 @@ public class Point {
     this.lat = lat;
     this.lon = lon;
   }
+  public double distance(Point p) {
+     return Math.sqrt((this.lat - p.lat) * (this.lat - p.lat) + (this.lon - p.lon) * (this.lon - p.lon));
+    }
 }
 
