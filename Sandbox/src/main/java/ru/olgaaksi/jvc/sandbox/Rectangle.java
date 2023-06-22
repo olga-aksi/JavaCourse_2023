8bb2a3a3cc935736075615f1f90e36f2d1f6e2c0
olga-aksi <olga-aksi@users.noreply.github.com>
2023-06-22 21:10:26 +0300

    Добавлены новые функции и классы

---
+++ Sandbox/src/main/java/ru/olgaaksi/jvc/sandbox/Rectangle.java
@@ -0,0 +1,6 @@
+package ru.olgaaksi.jvc.sandbox;
+
+public class Rectangle {
+  public double a;
+  public double b;
+}
---
+++ Sandbox/src/main/java/ru/olgaaksi/jvc/sandbox/Square.java
@@ -0,0 +1,5 @@
+package ru.olgaaksi.jvc.sandbox;
+
+public class Square {
+  public double l;
+}
