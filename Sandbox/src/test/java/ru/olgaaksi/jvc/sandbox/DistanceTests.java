65c479d33b4cbe356e201c367b0bac7cf6fb0f63
olga-aksi <olga-aksi@users.noreply.github.com>
2023-06-23 08:09:51 +0300

    Добавлены тесты для расстояния между точками

--- Sandbox/src/test/java/ru/olgaaksi/jvc/sandbox/DistanceTests.java
+++ Sandbox/src/test/java/ru/olgaaksi/jvc/sandbox/DistanceTests.java
@@ -10,4 +10,17 @@ public class DistanceTests {
     Point p2 = new Point(64, 14);
     Assert.assertEquals(p1.distance(p2), 41.43669871020132);
   }
+
+  @Test
+  public void testDistanceSamePoint() {
+    Point p1 = new Point(8, 8);
+    Assert.assertEquals(p1.distance(p1), 0.0);
+  }
+
+  @Test
+  public void testDistanceFromZeroPoint() {
+    Point p1 = new Point(8, 15);
+    Point p2 = new Point(0, 0);
+    Assert.assertEquals(p1.distance(p2), 17.0);
+  }
 }
