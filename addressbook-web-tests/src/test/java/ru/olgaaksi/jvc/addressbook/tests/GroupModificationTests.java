b72d9c11f3c0d837003b15dcab79f9d4c973532a
olga-aksi <olga-aksi@users.noreply.github.com>
2023-06-26 00:31:57 +0300

    Добавлены проверки метода type

--- addressbook-web-tests/src/test/java/ru/olgaaksi/jvc/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/olgaaksi/jvc/addressbook/tests/GroupModificationTests.java
@@ -9,7 +9,7 @@ public class GroupModificationTests extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("testnew", "test2new", "test3new"));
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
   }
