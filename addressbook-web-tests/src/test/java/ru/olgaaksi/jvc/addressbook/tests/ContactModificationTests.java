f55e2c34c90762b379ad3bf44a1404e1b8373d00
olga-aksi <olga-aksi@users.noreply.github.com>
2023-06-26 02:09:41 +0300

    Добавлены предусловия для модификации контакта

--- addressbook-web-tests/src/test/java/ru/olgaaksi/jvc/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/olgaaksi/jvc/addressbook/tests/ContactModificationTests.java
@@ -7,6 +7,9 @@ public class ContactModificationTests extends TestBase {
   @Test
   public void testContactModificationFromList() {
     app.getNavigationHelper().returnHome();
+    if (! app.getContactHelper().isThereAContact()) {
+      app.getContactHelper().createContact(new ContactData("Olga", "Mikhaylovna", "Alifanova", "Aksi", "QA engineer", "X5 Group", "Saint Petersburg, Komendantskiy 9", "+78123335566", "+79219997765", "olga.aksi@gmail.com", "14", "January", "1985", "[none]"));
+    }
     app.getNavigationHelper().gotoEditContact();
     app.getContactHelper().fillContactForm(new ContactData("Vladimir", "Iurievich", "Babkin", "Sotnik", "Security officer", "X5 Group", "Saint Petersburg, Komendantskiy 9", "+78123335566", "+79219997765", "olga.aksi@gmail.com", "09", "May", "1984", null), false);
     app.getContactHelper().updateContact();
@@ -15,6 +18,9 @@ public class ContactModificationTests extends TestBase {
   @Test
   public void testContactModificationFromInfo() {
     app.getNavigationHelper().returnHome();
+    if (! app.getContactHelper().isThereAContact()) {
+      app.getContactHelper().createContact(new ContactData("Olga", "Mikhaylovna", "Alifanova", "Aksi", "QA engineer", "X5 Group", "Saint Petersburg, Komendantskiy 9", "+78123335566", "+79219997765", "olga.aksi@gmail.com", "14", "January", "1985", "[none]"));
+    }
     app.getNavigationHelper().gotoContactDetails();
     app.getNavigationHelper().gotoModifyContact();
     app.getContactHelper().fillContactForm(new ContactData("iVladimir", "iIurievich", "iBabkin", "Sotnik", "Security officer", "X5 Group", "Saint Petersburg, Komendantskiy 9", "+78123335566", "+79219997765", "olga.aksi@gmail.com", "09", "May", "1984", null), false);
