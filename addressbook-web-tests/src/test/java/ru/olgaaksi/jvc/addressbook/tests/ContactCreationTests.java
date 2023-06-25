03db4e362ce0c9d752018d15fe568ce985be1049
olga-aksi <olga-aksi@users.noreply.github.com>
2023-06-26 02:06:50 +0300

    Добавлены предусловия для создания контакта с присвоенной группой

--- addressbook-web-tests/src/test/java/ru/olgaaksi/jvc/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/olgaaksi/jvc/addressbook/tests/ContactCreationTests.java
@@ -2,13 +2,24 @@ package ru.olgaaksi.jvc.addressbook.tests;
 
 import org.testng.annotations.*;
 import ru.olgaaksi.jvc.addressbook.model.ContactData;
+import ru.olgaaksi.jvc.addressbook.model.GroupData;
 
 public class ContactCreationTests extends TestBase {
-
   @Test
-  public void testContactCreation() throws Exception {
+  public void testContactWithGroupCreation() throws Exception {
+    app.getNavigationHelper().gotoGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getNavigationHelper().gotoContactCreation();
     app.getContactHelper().fillContactForm(new ContactData("Olga", "Mikhaylovna", "Alifanova", "Aksi", "QA engineer", "X5 Group", "Saint Petersburg, Komendantskiy 9", "+78123335566", "+79219997765", "olga.aksi@gmail.com", "14", "January", "1985", "test1"), true);
+    app.getContactHelper().submitContactCreation();
+    app.getNavigationHelper().returnHome();
+  }
+  @Test
+  public void testContactNoGroupCreation() throws Exception {
+    app.getNavigationHelper().gotoContactCreation();
+    app.getContactHelper().fillContactForm(new ContactData("Olga", "Mikhaylovna", "Alifanova", "Aksi", "QA engineer", "X5 Group", "Saint Petersburg, Komendantskiy 9", "+78123335566", "+79219997765", "olga.aksi@gmail.com", "14", "January", "1985", "[none]"), true);
     app.getContactHelper().submitContactCreation();
     app.getNavigationHelper().returnHome();
   }
@@ -29,7 +40,7 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testNoSurnameContactCreation() throws Exception {
     app.getNavigationHelper().gotoContactCreation();
-    app.getContactHelper().fillContactForm(new ContactData("Olga", "Mikhaylovna", "", "Aksi", "QA engineer", "X5 Group", "Saint Petersburg, Komendantskiy 9", "+78123335566", "+79219997765", "olga.aksi@gmail.com", "14", "January", "1985", "test1"), true);
+    app.getContactHelper().fillContactForm(new ContactData("Olga", "Mikhaylovna", "", "Aksi", "QA engineer", "X5 Group", "Saint Petersburg, Komendantskiy 9", "+78123335566", "+79219997765", "olga.aksi@gmail.com", "14", "January", "1985", "[none]"), true);
     app.getContactHelper().submitContactCreation();
     app.getNavigationHelper().returnHome();
   }
