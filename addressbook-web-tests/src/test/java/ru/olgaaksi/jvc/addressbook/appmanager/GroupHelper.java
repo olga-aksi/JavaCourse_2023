0a12a54eb2ce4053f3b14a5a938e231119ea4935
olga-aksi <olga-aksi@users.noreply.github.com>
2023-06-26 02:07:26 +0300

    Добавлены предусловия для удаления и изменения групп

--- addressbook-web-tests/src/test/java/ru/olgaaksi/jvc/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/olgaaksi/jvc/addressbook/appmanager/GroupHelper.java
@@ -48,4 +48,15 @@ public class GroupHelper extends HelperBase {
     click(By.xpath("//span[2]/input"));
     click(By.xpath("//span[3]/input"));
   }
+
+  public void createGroup(GroupData group) {
+    initGroupCreation();
+    fillGroupForm(group);
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+  public boolean isThereAGroup() {
+    return isElementPresent(By.name("selected[]"));
+  }
 }
--- addressbook-web-tests/src/test/java/ru/olgaaksi/jvc/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/olgaaksi/jvc/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.olgaaksi.jvc.addressbook.tests;
 
 import org.testng.annotations.*;
+import ru.olgaaksi.jvc.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
@@ -20,6 +24,11 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testsMultipleGroupDeletionFromList() {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectMultipleGroups();
     app.getGroupHelper().deleteSelectedGroups();
     app.getNavigationHelper().returnHome();
